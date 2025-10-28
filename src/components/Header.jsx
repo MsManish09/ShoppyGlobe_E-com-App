87c53934af489980b9983c0b527dcc730eadb490
MsManish09 <msmanish680@gmail.com>
2025-10-28 21:18:45 +0530

    minor header comp updae

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,12 @@ function Header(){
 
                 <nav className=" flex flex-col lg:flex-row md:flex-row justify-center lg:justify-evenly items-center h-full gap-4 " >
 
+                    {/* site logo */}
                     <img src="src/utils/shoppyGlobe_logo.png" alt="" className=" w-[20%] h-[40%] lg:h-[80%] " />
 
-                
+                    {/* navigation items */}
                     <ul className=" flex justify-evenly  items-center w-[75%] text-[1.2rem] font-semibold  " >
+                        {/* search comp inside nav only for desktop view */}
                         <li className=" w-[40%] hidden lg:flex " >Search</li>
                         <li className="navItems" >Home</li>
                         <li className="navItems" >Products</li>
