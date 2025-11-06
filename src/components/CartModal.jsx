1a5ed673a41e932317a8bd0bef6062d9d4ca47e3
MsManish09 <msmanish680@gmail.com>
2025-11-06 18:38:52 +0530

    minor design update in cart and modal

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -6,7 +6,7 @@ function CartModal({ message, onClose }) {
 
   return (
     <div className="fixed inset-0 flex justify-center items-center z-50 bg-[#93c5fdb3] text-blue-600">
-      <div className="bg-orange-100  p-8 rounded-[10px] shadow-2xl max-w-sm w-full text-center ">
+      <div className="bg-white  p-8 rounded-[10px] shadow-2xl max-w-sm w-full text-center ">
         <h2 className="text-2xl font-semibold  mb-4">{message}</h2>
 
         <button className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-800" onClick={onClose}>
