7e54eb00d1975ffb36c9172f360fe20b88ab3ceb
MsManish09 <msmanish680@gmail.com>
2025-12-09 18:28:57 +0530

    connected to mongodb altas

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
 
 import express from 'express'
+import mongoose from 'mongoose'
 
-// connect with mongoDB Atlas
 
 const app = new express()
+
+// connect with mongoDB-Atlas 
+mongoose.connect('mongodb+srv://shoppyGlobeDB:U82GAJGjDD5rZjcO@cluster0.i3bheai.mongodb.net/')
+
+    // db connection comformation
+    const db = mongoose.connection
+
+        db.on('open', ()=>{
+            console.log('DB is connnected')
+        })
+        db.on('error', ()=>{
+            console.log('DB connnection failed')
+        })
+
 
 app.listen(8080, (req, res)=>{
     console.log('Server is live...')
