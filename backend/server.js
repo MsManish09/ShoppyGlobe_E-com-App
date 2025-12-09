32931e954d5a2f59073972d4bea675797c5c00ba
MsManish09 <msmanish680@gmail.com>
2025-12-09 22:54:40 +0530

    .env file is added

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,26 @@
 
 import express from 'express'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+
+// load environment variable
+dotenv.config()
 
 const app = new express()
 
-// connect with mongoDB-Atlas 
-mongoose.connect('mongodb+srv://shoppyGlobeDB:U82GAJGjDD5rZjcO@cluster0.i3bheai.mongodb.net/')
+// connect with mongoDB-Atlas -> shoppyGlobe DB
+mongoose.connect(process.env.MONGODB_URL)
+.then( async()=>{ 
+    console.log('DB is connected')
 
+    // List of all collections in DB
+    const collections = await mongoose.connection.db.listCollections().toArray()
+    console.log("Collections:", collections.map(c => c.name))
+})
+.catch((err)=> console.log('DB Connnecton error', err))
+
+/* 
     // db connection comformation
     const db = mongoose.connection
 
@@ -18,6 +31,7 @@ mongoose.connect('mongodb+srv://shoppyGlobeDB:U82GAJGjDD5rZjcO@cluster0.i3bheai.
             console.log('DB connnection failed')
         })
 
+*/
 
 app.listen(8080, (req, res)=>{
     console.log('Server is live...')
